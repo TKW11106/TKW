1e517af2f4fdce5271c3925f23a14fcd1eaf15c2
Your Name <you@example.com>
2024-04-16 15:15:26 +0800

    first commit

---
+++ hi.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 10:47:35 2024
+
+@author: USER
+"""
+import streamlit as st
+
+st.set_page_config(
+    page_title="程式設計的頁面",    
+    page_icon=":white_check_mark:"
+)
+
+st.title("Main Page")
+st.sidebar.success("Select a page above!")
---
+++ pages/contact.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 14:52:46 2024
+
+@author: USER
+"""
+
+import streamlit as st
+
+st.title("Contacts")
